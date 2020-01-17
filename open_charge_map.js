d27fe6a1dde13051efa84d331a0142bd0599783b
Peter <k.fantoser@gmail.com>
2020-01-17 14:04:27 +0100

    Ask for location, and show points in a distance

--- open_charge_map.js
+++ open_charge_map.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
-    var uluru = {lat: 47.200000, lng: 19.040236};
+    var allowed = false;
+    var userPos = {lat: 47.200000, lng: 19.040236};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 7.5, center: uluru});
+        document.getElementById('map'), {zoom: 7.5, center: userPos});
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        allowed = true;
+        userPos.lat = position.coords.latitude;
+        userPos.lng = position.coords.longitude;
+        map.setCenter(userPos);
+        map.setZoom(10.5);
+    });
 
     $("#mapButt").on("click", mapRequest);
+      
 
     function mapRequest() {
         $("#mapButt").off("click", mapRequest);
@@ -35,7 +45,31 @@ $(document).ready(function(){
                 lat: markers[marker]["AddressInfo"]["Latitude"],
                 lng: markers[marker]["AddressInfo"]["Longitude"]
             };
-            new google.maps.Marker({position: position, map: map});
+            if (allowed) {
+                if (getDistanceFromLatLonInKm(userPos, position) < 100) {
+                    new google.maps.Marker({position: position, map: map});
+                }
+            } else {
+                new google.maps.Marker({position: position, map: map});
+            };
         }
+    }
+
+    function getDistanceFromLatLonInKm(pos1,pos2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(pos2.lat-pos1.lat);  // deg2rad below
+        var dLon = deg2rad(pos2.lng-pos1.lng); 
+        var a = 
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI/180);
     }
 });
